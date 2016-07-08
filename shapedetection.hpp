19b6596f11d0534b745fa49593169f0cce3bf750
shubhankar12 <shubhankar0109@gmail.com>
2016-07-08 00:37:12 -0700

    First commit

---
+++ shapedetection.hpp
@@ -0,0 +1,14 @@
+//
+//  shapedetection.hpp
+//  openLOL
+//
+//  Created by Shubhankar Agarwal on 4/1/16.
+//  Copyright © 2016 Shubhankar Agarwal. All rights reserved.
+//
+
+#ifndef shapedetection_hpp
+#define shapedetection_hpp
+
+#include <stdio.h>
+
+#endif /* shapedetection_hpp */

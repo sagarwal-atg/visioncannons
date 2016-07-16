3ba8fbfb12d2fc98d222f63a50feac4265fbd896
shubhankar12 <shubhankar0109@gmail.com>
2016-07-15 23:44:22 -0700

    tested mp4 video

--- main.cpp
+++ main.cpp
@@ -32,17 +32,17 @@ vector<double> area1;
 int main( int argc, char** argv )
 
 {
-    
+    // this will be the main file for tesing computer vision and deploying in submarine
     //Bouy_Ramming
     //underwatertest
     //Front/Image651.png
     cout<<"Time at starting is 0 sec"<<endl;
-    
+
     string filename = "/Users/Somi/Desktop/_Recordings_/underwatertest.mp4";
-    
+
     src = imread(filename);
-    
-    
+
+
     if(! src.data )                              // Check for invalid input
     {
         cout <<  "Could not open or find the image" << std::endl ;
@@ -50,88 +50,85 @@ int main( int argc, char** argv )
     }
     struct timeval tv0,tv1;
     gettimeofday(&tv0 , 0);
-    
 
-    
+
+
     detect_r = src.clone();
     detect_g = src.clone();
     detect_y = src.clone();
-    
+
     imshow("w", src);
-    
+
     Size s = src.size();
-    
-    
-    
+
+
+
     vector<vector<Point> > contours;
-    
-    
+
+
     gettimeofday(&tv1,0);
     long elap = (tv1.tv_sec - tv0.tv_sec ) * 100000  + tv1.tv_usec - tv0.tv_usec;
-    
+
     cout<<"Time after initial thresholding : " << elap/1000.0f <<"millisec"<<endl;
-    
+
     // red 150 , 100 , 150
     // green <  140 , 160 , 160
-    
+
     int framecounter = 0 ;
-    
+
     Mat final_image = Mat::zeros( src.size(), CV_8UC3 );
 
-    
+
  //   imshow("detect_r", detect_r);
     /////////////////////////////////////////////////////////////////////////////////////////////
     int color_bouy = filterImageHSV (detect_r ,src, s.height, s.width, cent_i , 150  , 100 , 150 );
-    
+
     fastNlMeansDenoising(detect_r , detect_r , 100, 3, 31);
   //  imshow( "Display red", detect_r );
 
-    
+
    vector<Point> red_vec = all_bouy(detect_r , final_image,   yawI ,  cent_i , area1 ,  thresh , max_thresh , rng ,  framecounter   , contours , 0);
     cout<<"red completed"<<endl;
     imshow( "after red completed" , final_image);
-    
+
     /////////////////////////////////////////////////////////////////////////////////////////////
  //    imshow("detect_g", detect_g);
         int color_bouy2 = filter_image_green(detect_g ,src, s.height, s.width, cent_i , 140  , 150 , 150 );
-    
+
         fastNlMeansDenoising(detect_g,detect_g , 100, 3, 31);
   //      imshow( "Display green", detect_g );
-    
+
     vector<Point> green_vec = all_bouy(detect_g ,final_image,  yawI ,  cent_i , area1 ,  thresh , max_thresh , rng ,  framecounter   , contours , 1);
     cout<<"green completed"<<endl;
     imshow( "after green completed" , final_image);
 
 
-    
+
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //     imshow("detect_y", detect_y);
         int color_bouy3 = filter_image(detect_y ,src, s.height, s.width, cent_i , 150  , 180 , 150 );
-    
 
-    
+
+
         fastNlMeansDenoising(detect_y,detect_y , 100, 3, 31);
        imshow( "Display yellow", detect_y );
-    
+
     vector<Point> yellow_vec = all_bouy(detect_y , final_image,   yawI ,  cent_i , area1 ,  thresh , max_thresh , rng ,  framecounter   , contours , 2);
     cout<<"yellow completed"<<endl;
     imshow( "after yellow completed" , final_image);
-    
+
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
-    
+
     for (int k = 0; k < contours.size(); k++) {
         cout<<"contour everything:" << contours[k]<<endl;
     }
  //   drawcontours( final_image  , contours ,  color_bouy , red_vec , green_vec , yellow_vec );
-    
+
    //   waitKey();
-    
+
 //imshow( "after denoising" , final_image);
-    
+
     waitKey();
-    
-    
+
+
 }
-
-
-
